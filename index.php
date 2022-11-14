a529b59ff0eec7696c7960a3139e616960003af9
gavinaperano <audesandrine6@gmail.com>
2022-11-14 11:14:06 +0000

    ok

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server of gavin aperano</title>
+</head>
+<body>
+    <h1>Hi wrong place</h1>
+    <ul>
+        <li> <a href ="http://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
+        <li><a href ="http://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
+        <li><a href ="http://gavinaperano.com/webapp/index.php">PORTFOLIO</a></li>
+        <li><a href ="http://gavinaperano.com/webapp/index.php">WEBSITE OF STREAMING</a></li>
+<!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
+
+    </ul>
+    
+</body>
+</html>
