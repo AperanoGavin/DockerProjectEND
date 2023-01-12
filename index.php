f9478c188544ac5b6d271f178679a039a8114fb1
GavinAperano <audesandrine6@gmail.com>
2023-01-12 11:28:31 +0000

    ok mail server

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <li><a href ="http://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
         <li><a href ="http://gavinaperano.com/webapp/index.php">PORTFOLIO</a></li>
         <li><a href ="http://gavinaperano.com/webapp/index.php">WEBSITE OF STREAMING</a></li>
+        <li><a href ="http://gavinaperano.com:8000/workflow/public/index.php">SYMFONY</a></li>
 <!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
 
     </ul>
