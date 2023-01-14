c3d5eaf1566282d74160b691ea97f44ec86e99e9
GavinAperano <audesandrine6@gmail.com>
2023-01-14 00:29:27 +0000

    update ssl

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
 <body>
     <h1>Hi wrong place</h1>
     <ul>
-        <li> <a href ="http://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
-        <li><a href ="http://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
-        <li><a href ="http://gavinaperano.com/webapp/index.php">PORTFOLIO</a></li>
-        <li><a href ="http://gavinaperano.com/webapp/index.php">WEBSITE OF STREAMING</a></li>
+        <li> <a href ="https://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
+        <li><a href ="https://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
+        <li><a href ="https://gavinaperano.com/webapp/index.php">PORTFOLIO</a></li>
+        <li><a href ="https://gavinaperano.com/webapp/index.php">WEBSITE OF STREAMING</a></li>
         <li><a href ="http://gavinaperano.com:8000/workflow/public/index.php">SYMFONY</a></li>
 <!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
 
