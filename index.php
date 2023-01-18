55b5944c42e3fca4abd8015bcc51031d5827c4f3
GavinAperano <audesandrine6@gmail.com>
2023-01-18 10:50:11 +0000

    upgrade

--- index.php
+++ index.php
@@ -16,8 +16,10 @@
     <ul>
         <li> <a href ="https://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
         <li><a href ="https://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
-        <li><a href ="https://gavinaperano.com/stream.php">WEBSITE OF STREAMING</a></li>
-        <li><a href ="http://gavinaperano.com:8000/workflow/public/index.php">SYMFONY</a></li>
+        <li><a href ="https://gavinaperano.com/stream/index.php">WEBSITE OF STREAMING</a></li>
+        <li><a href ="http://gavinaperano.com:8000/workflow/public/index.php">SYMFONY API MEME</a></li>
+        <li><a href ="https://gavinaperano.me/">FRAMEWORK CSS</a></li>
+
 <!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
 
     </ul>
