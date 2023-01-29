e586b63d3faa763a1b24eda0a3693d94838316dc
GavinAperano <audesandrine6@gmail.com>
2023-01-29 12:18:18 +0000

    update link

--- index.php
+++ index.php
@@ -14,10 +14,14 @@
 <body>
     <h1>Hi wrong place</h1>
     <ul>
+
+        <li style="float:right; margin-right: 10px;"><a href ="http://gavinaperano.com:3001/dashboard">MONITORING( please don't edit anythings)</a></li>
         <li> <a href ="https://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
         <li><a href ="https://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
-        <li><a href ="https://gavinaperano.com/stream/index.php">WEBSITE OF STREAMING</a></li>
-        <li><a href ="http://gavinaperano.com:8000/workflow/public/index.php">SYMFONY API MEME</a></li>
+        <li><a href ="http://gavinaperano.com/stream/index.php">WEBSITE OF STREAMING</a></li>
+        <li><a href ="https://gavinaperano.com:8800/">SCAN FOOD CODE BAR</a></li>
+        <li><a href ="http://gavinaperano.com:800/workflow/public/index.php">SYMFONY API MEME</a></li>
+        <li><a href ="http://gavinaperano.com:8000/">WEBSITE OF REAL TIME CHAT</a></li>
         <li><a href ="https://gavinaperano.me/">FRAMEWORK CSS</a></li>
 
 <!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
