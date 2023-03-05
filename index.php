b89c4a628aaea6ed2d0e54c7c1965dcf60df31c6
GavinAperano <audesandrine6@gmail.com>
2023-03-05 13:34:41 +0000

    upgrade

--- index.php
+++ index.php
@@ -19,10 +19,12 @@
         <li> <a href ="https://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
         <li><a href ="https://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
         <li><a href ="http://gavinaperano.com/stream/index.php">WEBSITE OF STREAMING</a></li>
-        <li><a href ="https://gavinaperano.com:8800/">SCAN FOOD CODE BAR</a></li>
+        <li><a href ="https://gavinaperano.com:90/">SCAN FOOD CODE BAR</a></li>
         <li><a href ="http://gavinaperano.com:800/workflow/public/index.php">SYMFONY API MEME</a></li>
         <li><a href ="http://gavinaperano.com:8000/">WEBSITE OF REAL TIME CHAT</a></li>
-        <li><a href ="https://gavinaperano.me/">FRAMEWORK CSS</a></li>
+        <li><a href ="http://gavinaperano.me/">FRAMEWORK CSS</a></li>
+        <li><a href ="http://46.226.107.16:5173">ABDOU </a></li>
+
 
 <!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
 
