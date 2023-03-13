947bb96f3f5c1636799ffa14ce64620d10de60e6
GavinAperano <audesandrine6@gmail.com>
2023-03-13 15:29:53 +0000

    update website $

--- index.php
+++ index.php
@@ -1,35 +1,54 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Server of gavin aperano</title>
-
-     
+    <title>Document</title>
 </head>
 <body>
-    <h1>Hi wrong place</h1>
-    <ul>
-
-        <li style="float:right; margin-right: 10px;"><a href ="http://gavinaperano.com:3001/dashboard">MONITORING( please don't edit anythings)</a></li>
-        <li> <a href ="https://gavinaperano.com/webapp/index.php">WEBSITE OF ECOMMERCE</a></li>
-        <li><a href ="https://gavinaperano.com/Html:Css/index.html">WEBSITE TYPE HTML CSS</a></li>
-        <li><a href ="http://gavinaperano.com/stream/index.php">WEBSITE OF STREAMING</a></li>
-        <li><a href ="https://gavinaperano.com:90/">SCAN FOOD CODE BAR</a></li>
-        <li><a href ="http://gavinaperano.com:800/workflow/public/index.php">SYMFONY API MEME</a></li>
-        <li><a href ="http://gavinaperano.com:8000/">WEBSITE OF REAL TIME CHAT</a></li>
-        <li><a href ="http://gavinaperano.me/">FRAMEWORK CSS</a></li>
-        <li><a href ="http://46.226.107.16:5173">ABDOU </a></li>
-
-
-<!--    <li><a href ="http://gavinaperano.com/Vid/RPReplay_Final1658994359-_1_.mp4">ça marche pas</a></li>-->
-
-    </ul>
     
+
+<div class="container bootstrap snippets bootdey">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="pull-right" style="margin-top:10px;">
+                <div class="col-md-10 col-md-offset-1 pull-right">
+                <a href="http://gavinaperano.com:3000/" class="btn btn-primary btn-lg">
+                    <img class="img-error" src="./hom.png">
+                </a>
+                    <h2>Clique sur Hommer</h2>
+                    <p>Attention il n'aime pas les chatouilles!</p>
+                    <div class="error-actions">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    body {
+    background: #2E95EF;
+    background-image: -moz-radial-gradient(center 45deg,circle cover, #e5345b, #2E95EF);
+    background-image: -webkit-gradient(radial, 50% 50%, 0, 50% 50%,800, from(#e5345b), to(#2E95EF));
+    padding-top: 15%;
     
+}
+.img-error{
+width:500px;
+height:300px;
+}
+.container{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+p , h2{
+    text-align: center;
+}
+</style>
+
 </body>
 </html>
