28c93a5d4726fa6673714849dc583353ca2d2652
GavinAperano <audesandrine6@gmail.com>
2023-03-18 15:19:03 +0000

    update

---
+++ _index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div class="container bootstrap snippets bootdey">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="pull-right" style="margin-top:10px;">
+                <div class="col-md-10 col-md-offset-1 pull-right">
+                <a href="http://gavinaperano.com:3000/" class="btn btn-primary btn-lg">
+                    <img class="img-error" src="./hom.png">
+                </a>
+                    <h2>Clique sur Hommer</h2>
+                    <p>Attention il n'aime pas les chatouilles!</p>
+                    <div class="error-actions">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    body {
+    background: #2E95EF;
+    background-image: -moz-radial-gradient(center 45deg,circle cover, #e5345b, #2E95EF);
+    background-image: -webkit-gradient(radial, 50% 50%, 0, 50% 50%,800, from(#e5345b), to(#2E95EF));
+    padding-top: 15%;
+    
+}
+.img-error{
+width:500px;
+height:300px;
+}
+.container{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+p , h2{
+    text-align: center;
+}
+</style>
+
+</body>
+</html>
